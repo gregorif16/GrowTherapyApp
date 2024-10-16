3a050b326e282ef1bc2dcd428477e56f3cf78e6a
Gregori Farias <gregori.f16@gmail.com>
2024-10-16 06:03:42 -0400

    Initial Commit

---
+++ GrowTherapyApp/GrowTherapyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GrowTherapyAppApp.swift
+//  GrowTherapyApp
+//
+//  Created by Gregori Farias on 16/10/24.
+//
+
+import SwiftUI
+
+@main
+struct GrowTherapyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
